f2bb4edd198e547de6d8b58f72aae8a45bf6f61a
jankanapka <filiposn@gmail.com>
2017-06-28 10:50:14 +0200

    working menu

---
+++ app/src/main/java/com/example/filip/alcocalco/BetterActivity.java
@@ -0,0 +1,40 @@
+package com.example.filip.alcocalco;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class BetterActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_better);
+        Intent intent = getIntent();
+    }
+
+    public void calculate(View view)
+    {
+        EditText price1 = (EditText) findViewById(R.id.Price1);
+        EditText price2 = (EditText) findViewById(R.id.Price2);
+        EditText volume1 = (EditText) findViewById(R.id.Volume1);
+        EditText volume2 = (EditText) findViewById(R.id.Volume2);
+        AlcoType alc1 = new AlcoType(Double.parseDouble(price1.getText().toString()), Double.parseDouble(volume1.getText().toString()));
+        AlcoType alc2 = new AlcoType(Double.parseDouble(price2.getText().toString()), Double.parseDouble(volume2.getText().toString()));
+        TextView result = (TextView) findViewById(R.id.Answer);
+        String answer;
+        if(alc1.getPpv() < alc2.getPpv())
+            result.setText("Second is better!");
+        else if(alc2.getPpv() < alc1.getPpv())
+            result.setText("First is better!");
+        else
+            result.setText("Equal");
+
+
+    }
+}
--- app/src/main/java/com/example/filip/alcocalco/MainActivity.java
+++ app/src/main/java/com/example/filip/alcocalco/MainActivity.java
@@ -1,40 +1,62 @@
 package com.example.filip.alcocalco;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import static android.R.attr.id;
+import static android.R.id.list;
+
+//main menu
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private String[] options = {"AlcoCalco", "PromileCalco", "AlcoMap", "Credits", "Quit"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options);
+        ListView list = (ListView) findViewById(R.id.menu);
+        list.setAdapter(adapter);
+        AdapterView.OnItemClickListener clickHandler = new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                if (position == 0)
+                {
+                    Intent intent = new Intent(MainActivity.this, BetterActivity.class);
+                    startActivity(intent);
+                }
+
+                if (position == 4)
+                {
+                    finish();
+                    System.exit(0);
+                }
+            }
+        };
+        list.setOnItemClickListener(clickHandler);
+
+
+
     }
 
-    public void calculate(View view)
-    {
-        EditText price1 = (EditText) findViewById(R.id.Price1);
-        EditText price2 = (EditText) findViewById(R.id.Price2);
-        EditText volume1 = (EditText) findViewById(R.id.Volume1);
-        EditText volume2 = (EditText) findViewById(R.id.Volume2);
-
-        AlcoType alc1 = new AlcoType(Double.parseDouble(price1.getText().toString()), Double.parseDouble(volume1.getText().toString()));
-        AlcoType alc2 = new AlcoType(Double.parseDouble(price2.getText().toString()), Double.parseDouble(volume2.getText().toString()));
-        TextView result = (TextView) findViewById(R.id.Answer);
-        String answer;
-        if(alc1.getPpv() < alc2.getPpv())
-            result.setText("Second is better!");
-        else if(alc2.getPpv() < alc1.getPpv())
-            result.setText("First is better!");
-        else
-            result.setText("Equal");
 
 
-    }
 }
 
