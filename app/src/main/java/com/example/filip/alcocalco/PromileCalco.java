1a29d9383b4a55363b49bec4f7081c67114ebbcb
jankanapka <filiposn@gmail.com>
2017-07-11 11:53:32 +0200

    changed list size

--- app/src/main/java/com/example/filip/alcocalco/PromileCalco.java
+++ app/src/main/java/com/example/filip/alcocalco/PromileCalco.java
@@ -1,9 +1,18 @@
 package com.example.filip.alcocalco;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -12,7 +21,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PromileCalco extends AppCompatActivity
+public class PromileCalco extends Activity
 {
 
 
@@ -27,6 +36,34 @@ public class PromileCalco extends AppCompatActivity
         list = (ListView) findViewById(R.id.drinkListView);
         adapter = new AlcoAdapter(getApplicationContext(), R.layout.row_layout);
         list.setAdapter(adapter);
+
+        AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+            {
+
+                //Intent intent = new Intent(getApplicationContext(), AlcoEditDialogActivity.class);
+                //startActivity(intent);
+                final Dialog editDialog = new Dialog(PromileCalco.this);
+                editDialog.setContentView(R.layout.alco_edit_dialog);
+
+                Button cancelButton = (Button) findViewById(R.id.dialogCancel);
+                cancelButton.setOnClickListener(new View.OnClickListener() //TODO REPAIR IT
+                {
+                    @Override
+                    public void onClick(View view)
+                    {
+                        editDialog.dismiss();
+                    }
+                });
+
+                Button okButton = (Button) findViewById(R.id.dialogOk);
+
+                editDialog.show();
+            }
+        };
+        list.setOnItemClickListener(clickListener);
     }
 
     private List drinkList = new ArrayList<AlcoType>();
